43f7d110161962ba9d81fd7bd26bfad409917f30
Harshit Luthra <love.coolsachin@gmail.com>
2017-09-27 03:54:44 +0530

    Add login_main.xml

---
+++ app/src/main/java/luthra/harshit/androidquizappwithfirebase/modal/User.java
@@ -0,0 +1,42 @@
+package luthra.harshit.androidquizappwithfirebase.modal;
+
+/**
+ * Created by crappy on 27/9/17.
+ */
+
+public class User {
+    private String userName, password, email;
+
+    public User() {
+    }
+
+    public User(String userName, String password, String email) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
